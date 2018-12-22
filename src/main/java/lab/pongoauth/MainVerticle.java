ed9b919d275e7ab6a8cb8c25026711bf58be71a4
Anderson Carvalho <anderson.mo.carvalho@gmail.com>
2018-12-22 22:45:20 +0000

    Start the web application asynchronously
    FUTURE WEB ASYNCHRONOUSLY

--- src/main/java/lab/pongoauth/MainVerticle.java
+++ src/main/java/lab/pongoauth/MainVerticle.java
@@ -4,6 +4,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -18,14 +19,22 @@ public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start() throws Exception {
+    startWebApplication();
+  }
+
+  private Future<Void> startWebApplication(){
+    Future<Void> future = Future.future();
     vertx.createHttpServer().requestHandler(r -> {
       r.response().end("pong");
     }).listen(port, result -> {
       if (result.succeeded()) {
         LOGGER.info("MainVerticle started");
+        future.complete();
       } else {
         LOGGER.log(Level.SEVERE, "MainVerticle failed to start", result.cause());
+        future.fail(result.cause());
       }
     });
+    return future;
   }
 }
